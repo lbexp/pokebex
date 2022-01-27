010d280585673c0cf167ba8c6bb8d88ed8f06e99
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-28 01:46:17 +0800

    refactor: navbar organism component

--- src/components/organisms/navbar/index.js
+++ src/components/organisms/navbar/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Menu } from 'components/molecules/nav';
+import { NavWrapper } from 'components/atoms/nav';
+import { NavLogo, NavMenu } from 'components/molecules/nav';
 
-export default function Navbar({ menu }) {
+export default function Navbar({ logo, menus }) {
   return (
-    <Menu
-      links={menu}
-    />
+    <NavWrapper>
+      <NavLogo
+        href={logo.href}
+        width={logo.width}
+        height={logo.height}
+        src={logo.src}
+      />
+      <NavMenu
+        links={menus}
+      />
+    </NavWrapper>
   );
 };
