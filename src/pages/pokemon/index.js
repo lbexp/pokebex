9093d7d528646ae6bc02fceec3fecfb35a53781b
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-28 01:47:19 +0800

    refactor: pokemon index page

--- src/pages/pokemon/index.js
+++ src/pages/pokemon/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Navbar from 'components/organisms/navbar';
+import PokemonLogo from 'assets/images/pokemon_logo.png';
 
 export default function Index() {
   return (
     <div>
-      Index Pokemon
+      <Navbar
+        logo={{ href: '/', src: PokemonLogo, width: 125, height: null }}
+        menus={[
+          { href: '/', children: 'List' },
+          { href: '/my-pokemon', children: 'Mine' },
+        ]}
+      />
     </div>
   )
 }
