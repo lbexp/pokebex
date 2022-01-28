de5a931306e2abd06fda2f56da1592a574caa288
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-28 23:49:01 +0800

    feat: get pokemons query in pokemon index page

--- src/pages/pokemon/index.js
+++ src/pages/pokemon/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import PokemonIndex from 'components/templates/PokemonIndex';
 import PokemonLogo from 'assets/images/pokemon_logo.png';
+import { useQuery } from "@apollo/client";
+import { GET_POKEMONS } from 'graphql/get-pokemons';
 
-export default function Index() {
+function Index() {
+  const { loading, error, data } = useQuery(GET_POKEMONS, {
+    variables: { limit: 10, offset: 1 },
+  });
+
   return (
     <PokemonIndex
       navbar={{
@@ -12,11 +18,14 @@ export default function Index() {
           { href: '/my-pokemon', children: 'Collection' },
         ]
       }}
-      items={[
-        { title: 'Geodude', subtitle: 'Dude', img: { src: PokemonLogo, alt: 'Geodude' } },
-        { title: 'Geodude', subtitle: 'Dude', img: { src: PokemonLogo, alt: 'Geodude' } },
-        { title: 'Geodude', subtitle: 'Dude', img: { src: PokemonLogo, alt: 'Geodude' } },
-      ]}
+      items={{ loading: loading, error: error, data: data?.pokemons?.results }}
+      // items={[
+      //   { title: 'Geodude', subtitle: 'Dude', img: { src: PokemonLogo, alt: 'Geodude' } },
+      //   { title: 'Geodude', subtitle: 'Dude', img: { src: PokemonLogo, alt: 'Geodude' } },
+      //   { title: 'Geodude', subtitle: 'Dude', img: { src: PokemonLogo, alt: 'Geodude' } },
+      // ]}
     />
   )
 };
+
+export default Index;
