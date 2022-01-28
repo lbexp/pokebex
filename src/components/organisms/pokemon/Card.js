5d968276bec949a0bb4dd81d24ad3f4607f5b0e7
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-29 00:58:04 +0800

    fix: pokemon card organism component

--- src/components/organisms/pokemon/Card.js
+++ src/components/organisms/pokemon/Card.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import { BaseCard } from 'components/atoms/card';
 import { BaseImage } from 'components/atoms/image';
+import { AnchorWrapper } from 'components/atoms/anchor';
 import { FlexContainer } from 'components/atoms/container';
 import { PokemonHeader } from 'components/molecules/pokemon';
 
-export default function Header({ header, image }) {
+export default function Card({ id, header, image }) {
   return (
-    <BaseCard
-      padding="10px"
-    >
-      <FlexContainer
-        align="center"
-        justify="center"
-        direction="column"
-        gap="10px"
+    <AnchorWrapper href={`/pokemon/${id}`}>
+      <BaseCard
+        padding="10px"
       >
-        <PokemonHeader
-          title={header.title}
-          subtitle={header.subtitle}
-        />
-        <BaseImage
-          src={image.src}
-          alt={image.alt}
-          width="250px"
-        />
-      </FlexContainer>
-    </BaseCard>
+        <FlexContainer
+          align="center"
+          justify="center"
+          direction="column"
+          gap="10px"
+        >
+          <PokemonHeader
+            title={header.title}
+            subtitle={header.subtitle}
+          />
+          <BaseImage
+            src={image.src}
+            alt={image.alt}
+            width="250px"
+          />
+        </FlexContainer>
+      </BaseCard>
+    </AnchorWrapper>
   )
 };
