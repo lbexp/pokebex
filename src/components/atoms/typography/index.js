931629aedc2a564184542df37115cc5f5b092425
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-29 03:12:28 +0800

    feat: heading four typography atom component

--- src/components/atoms/typography/index.js
+++ src/components/atoms/typography/index.js
@@ -1,9 +1,11 @@
 import HeadingOne from './HeadingOne';
 import HeadingTwo from './HeadingTwo';
 import HeadingThree from './HeadingThree';
+import HeadingFour from './HeadingFour';
 
 export {
   HeadingOne,
   HeadingTwo,
   HeadingThree,
+  HeadingFour,
 };
