96ae3998ec45ccfe24dccdeeab07d1662dc4be23
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-28 17:27:02 +0800

    fix: pokemon index template

--- src/components/templates/PokemonIndex.js
+++ src/components/templates/PokemonIndex.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import Navbar from 'components/organisms/navbar';
+import { PokemonCard } from 'components/organisms/pokemon';
+import { GridContainer } from 'components/atoms/container';
 
-export default function PokemonIndex({ navbar }) {
+export default function PokemonIndex({ navbar, items }) {
   return (
-    <div>
-      <Navbar
-        logo={navbar.logo}
-        menus={navbar.menus}
-      />
-    </div>
+    <>
+      <header>
+        <Navbar
+          logo={navbar.logo}
+          menus={navbar.menus}
+        />
+      </header>
+      <main>
+        <GridContainer
+          padding="20px 15px"
+          gap="15px"
+        >
+          { items.map((item, index) => (
+            <PokemonCard
+              key={index}
+              header={{ title: item.title, subtitle: item.subtitle }}
+              image={{ src: item.img.src, alt: item.img.alt }}
+            />
+          ))}
+        </GridContainer>
+      </main>
+    </>
   )
 };
