c2dc1592d566b17e9c54dfe1cac3807390207d62
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-29 01:17:48 +0800

    fix: loader classname

--- src/components/atoms/loader/index.js
+++ src/components/atoms/loader/index.js
@@ -2,6 +2,6 @@ import './style.css';
 
 export function Loader() {
   return(
-    <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+    <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
   )
 };
