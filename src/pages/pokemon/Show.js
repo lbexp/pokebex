d4901c1833a99c27e255764405c78027102b7b09
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-29 03:13:15 +0800

    feat: show pokemon page

--- src/pages/pokemon/Show.js
+++ src/pages/pokemon/Show.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import PokemonShow from 'components/templates/pokemon/Show';
+import PokemonLogo from 'assets/images/pokemon_logo.png';
+import { useQuery } from "@apollo/client";
+import { SHOW_POKEMONS } from 'graphql/show-pokemon';
 
-export default function Show() {
+function Show() {
+  const { loading, error, data } = useQuery(SHOW_POKEMONS, {
+    variables: { name: 'venusaur' },
+  });
+
   return (
-    <div>
-      Show Pokemon
-    </div>
+    <PokemonShow
+      navbar={{
+        logo: { href: '/', src: PokemonLogo, width: 125, height: null },
+        menus: [
+          { href: '/', children: 'List' },
+          { href: '/my-pokemon', children: 'Collection' },
+        ]
+      }}
+      item={{ loading: loading, error: error, data: data?.pokemon }}
+    />
   )
-}
+};
+
+export default Show;
