4fe63bd59c823fc06a5045e4c3166744f44ee4d5
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-29 03:11:57 +0800

    fix: base card atom component

--- src/components/atoms/card/Base.js
+++ src/components/atoms/card/Base.js
@@ -1,12 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 
-export default function Base({ children, position, padding, borderRadius, boxShadow }) {
+export default function Base({ children, position, padding, margin, borderRadius, boxShadow }) {
   return (
     <div
       css={css`
         position: ${position};
         padding: ${padding};
+        margin: ${margin};
         border-radius: ${borderRadius};
         box-shadow: ${boxShadow};
       `}
@@ -19,6 +20,7 @@ export default function Base({ children, position, padding, borderRadius, boxSha
 Base.defaultProps = {
   position: 'relative',
   padding: '10px',
+  margin: '0px',
   borderRadius: '10px',
   boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.1)',
 };
