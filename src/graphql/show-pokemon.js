181ddae70d358f8abf12356474ee7a035af3fd40
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-29 01:31:30 +0800

    fix: show pokemon graphql query

---
+++ src/graphql/show-pokemon.js
@@ -0,0 +1,23 @@
+import { gql } from "@apollo/client";
+
+export const SHOW_POKEMONS = gql`
+  query pokemon($name: String!) {
+    pokemon(name: $name) {
+      id
+      name
+      sprites {
+        front_default
+      }
+      moves {
+        move {
+          name
+        }
+      }
+      types {
+        type {
+          name
+        }
+      }
+    }
+  }
+`;
