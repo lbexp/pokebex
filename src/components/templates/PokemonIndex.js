e907fbc87a10b26f337757a8bbf53a0712d00673
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-29 00:59:29 +0800

    refactor: pokemon index template

--- src/components/templates/PokemonIndex.js
+++ src/components/templates/PokemonIndex.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from 'components/organisms/navbar';
 import { PokemonCard } from 'components/organisms/pokemon';
 import { GridContainer } from 'components/atoms/container';
+import { Loader } from 'components/atoms/loader';
 
 export default function PokemonIndex({ navbar, items }) {
   return (
@@ -12,18 +13,19 @@ export default function PokemonIndex({ navbar, items }) {
           menus={navbar.menus}
         />
       </header>
-      <main>
+      <main style={{ textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         {
           items.loading ?
-            <p>Loading...</p>
+            <Loader />
           :
             <GridContainer
               padding="20px 15px"
               gap="15px"
             >
-              { items.data?.map((item, index) => (
+              { items.data?.map((item) => (
                 <PokemonCard
-                  key={index}
+                  key={item.id}
+                  id={item.id}
                   header={{ title: item.name, subtitle: '0 Owned' }}
                   image={{ src: item.image, alt: item.name }}
                 />
