bd89e1ce58f9e7d14476f3888160ef693976ba43
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-28 17:07:27 +0800

    fix: default props on base card atom component

--- src/components/atoms/card/Base.js
+++ src/components/atoms/card/Base.js
@@ -1,16 +1,24 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 
-export default function Base({ children }) {
+export default function Base({ children, position, padding, borderRadius, boxShadow }) {
   return (
     <div
       css={css`
-        position: relative;
-        border-radius: 10px;
-        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.1);
+        position: ${position};
+        padding: ${padding};
+        border-radius: ${borderRadius};
+        box-shadow: ${boxShadow};
       `}
     >
       { children }
     </div>
   )
+};
+
+Base.defaultProps = {
+  position: 'relative',
+  padding: '10px',
+  borderRadius: '10px',
+  boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.1)',
 };
