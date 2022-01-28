e989789f6040342af485212562a9f5e6d4c4c36d
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-28 23:47:54 +0800

    refactor: app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import {
-  BrowserRouter, Routes, Route,
-} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import routes from 'routes';
-// import './App.css';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          {routes.map((route, index) => (
-              <Route
-                key={index}
-                path={route.path}
-                element={<route.component />}
-              /> 
-          ))}
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              element={<route.component />}
+            /> 
+        ))}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
