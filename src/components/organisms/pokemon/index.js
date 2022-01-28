6da768a36d71f76631b69b5b56ce0af25b4c88ea
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-29 03:13:05 +0800

    feat: template for show pokemon

--- src/components/organisms/pokemon/index.js
+++ src/components/organisms/pokemon/index.js
@@ -1,5 +1,7 @@
 import PokemonCard from './Card';
+import PokemonDetail from './Detail';
 
 export {
   PokemonCard,
+  PokemonDetail,
 };
---
+++ src/components/templates/pokemon/Show.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Loader } from 'components/atoms/loader';
+import Navbar from 'components/organisms/navbar';
+import { PokemonDetail } from 'components/organisms/pokemon'
+
+export default function PokemonIndex({ navbar, item }) {
+  return (
+    <>
+      <header>
+        <Navbar
+          logo={navbar.logo}
+          menus={navbar.menus}
+        />
+      </header>
+      <main 
+        style={
+          item.loading ? { 
+            textAlign: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          } 
+          : null
+        }
+      >
+        {
+          item.loading ?
+            <Loader />
+          :
+            <PokemonDetail />
+        }
+      </main>
+    </>
+  )
+};
