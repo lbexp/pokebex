f62afaf86272c69b374697e8850b788314612f7e
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-29 15:31:09 +0800

    refactor: pokemon show page

--- src/pages/pokemon/Show.js
+++ src/pages/pokemon/Show.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { useParams } from "react-router-dom";
 import PokemonShow from 'components/templates/pokemon/Show';
 import PokemonLogo from 'assets/images/pokemon_logo.png';
 import { useQuery } from "@apollo/client";
 import { SHOW_POKEMONS } from 'graphql/show-pokemon';
 
 function Show() {
+  const { name } = useParams();
   const { loading, error, data } = useQuery(SHOW_POKEMONS, {
-    variables: { name: 'venusaur' },
+    variables: { name: name },
   });
 
   return (
@@ -18,7 +20,22 @@ function Show() {
           { href: '/my-pokemon', children: 'Collection' },
         ]
       }}
-      item={{ loading: loading, error: error, data: data?.pokemon }}
+      item={{
+        loading: loading,
+        error: error,
+        data: {
+          header: {
+            title: data?.pokemon.name,
+            subtitle: '0 Owned'
+          },
+          image: {
+            src: data?.pokemon.sprites.front_default,
+            alt: data?.pokemon.name
+          },
+          types: data?.pokemon.types.map((type) => type.type.name),
+          moves: data?.pokemon.moves.map((move) => move.move.name)
+        }
+      }}
     />
   )
 };
