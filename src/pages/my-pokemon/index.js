f92915096a257e28da84baebe67dc542c8283f75
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-30 00:29:27 +0800

    feat: my pokemon index

--- src/pages/my-pokemon/index.js
+++ src/pages/my-pokemon/index.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLiveQuery } from 'dexie-react-hooks';
+import { pokebexIdb } from 'data/pokebex-idb';
+import PokemonIndex from 'components/templates/pokemon';
 
-export default function Index() {
+function Index() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [catchedPokemons, setCatchedPokemons] = useState([]);
+
+  useLiveQuery(async() => {
+    try {
+      setIsLoading(true);
+      const queryPokemons =  await pokebexIdb.pokemons.toArray();
+      setCatchedPokemons(queryPokemons);
+      setIsLoading(false);
+    } catch(error) {
+      setError(error);
+    }
+  });
+
   return (
-    <div>
-      My Pokemons
-    </div>
+    <PokemonIndex
+      navbar={{
+        menus: [
+          { href: '/', children: 'List' },
+          { href: '/my-pokemon', children: 'Collection' },
+        ]
+      }}
+      items={{ loading: isLoading, error: error, data: catchedPokemons }}
+    />
   )
-}
+};
+
+export default Index;
