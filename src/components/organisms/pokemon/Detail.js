ae85c33238941f85038e0ba4ea9dcaaef22a554c
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-29 15:30:17 +0800

    refactor: pokemon detail organism

--- src/components/organisms/pokemon/Detail.js
+++ src/components/organisms/pokemon/Detail.js
@@ -1,74 +1,28 @@
 import React from 'react';
-import { BaseCard } from 'components/atoms/card';
-import { BaseLabel } from 'components/atoms/label';
-import { BaseContainer, FlexContainer } from 'components/atoms/container';
-import { BaseImage } from 'components/atoms/image';
-import { HeadingFour } from 'components/atoms/typography';
-import { PokemonHeader } from 'components/molecules/pokemon';
+import { BaseContainer } from 'components/atoms/container';
+import { PokemonCard, PokemonDescSlider } from 'components/molecules/pokemon';
 
-export default function Detail({  }) {
+export default function Detail({ data }) {
   return (
     <BaseContainer
-      // direction="column"
       padding="20px"
-      // gap="30px"
     >
       <BaseContainer
         margin="0px 0px 30px 0px"
       >
-        <BaseCard>
-          <FlexContainer
-            align="center"
-            justify="center"
-            direction="column"
-            gap="10px"
-          >
-            <PokemonHeader
-              title="Halo"
-              subtitle="Hola"
-            />
-            <BaseImage
-              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png"
-              alt="Yo"
-              width="250px"
-            />
-          </FlexContainer>
-        </BaseCard>
+        <PokemonCard
+          header={data.header}
+          image={data.image}
+        />
       </BaseContainer>
-      <BaseContainer
-        margin="0px 0px 30px 0px"
-      >
-        <BaseContainer
-          margin="0px 0px 10px 0px"
-        >
-          <HeadingFour>Types:</HeadingFour>
-        </BaseContainer>
-        <FlexContainer
-          justify="unset"
-          gap="10px"
-        >
-          { [1, 2, 3].map((item) => (
-            <BaseLabel>Test</BaseLabel>
-          )) }
-        </FlexContainer>
-      </BaseContainer>
-      <BaseContainer
-        margin="0px 0px 30px 0px"
-      >
-        <BaseContainer
-          margin="0px 0px 10px 0px"
-        >
-          <HeadingFour>Moves:</HeadingFour>
-        </BaseContainer>
-        <FlexContainer
-          justify="unset"
-          gap="10px"
-        >
-          { [1, 2, 3].map((item) => (
-            <BaseLabel>Test</BaseLabel>
-          )) }
-        </FlexContainer>
-      </BaseContainer>
+      <PokemonDescSlider 
+        title="Types :"
+        items={data.types}
+      />
+      <PokemonDescSlider 
+        title="Moves :"
+        items={data.moves}
+      />
     </BaseContainer>
   )
 };
