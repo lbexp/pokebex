0b1cd58a628fe3b62eac356aacb6ce8a7cd3e157
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-29 15:29:53 +0800

    fix: import pokemon molecule

--- src/components/molecules/pokemon/index.js
+++ src/components/molecules/pokemon/index.js
@@ -1,5 +1,9 @@
 import PokemonHeader from './Header';
+import PokemonCard from './Card';
+import PokemonDescSlider from './DescSlider';
 
 export {
   PokemonHeader,
+  PokemonCard,
+  PokemonDescSlider,
 };
