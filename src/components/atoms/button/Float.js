ef41fdde546d7b479e47087cc60db30142a42d8e
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-29 17:31:35 +0800

    feat: floating button atom component

---
+++ src/components/atoms/button/Float.js
@@ -0,0 +1,34 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+export default function Float({ children, img }) {
+  return (
+    <label
+      css={css`
+        font-size: 18px;
+        position: fixed;
+        bottom: 16px;
+        right: 16px;
+        ${
+          img ?
+            `background-image: url(${img});
+            background-repeat: no-repeat;
+            background-size: 55px 55px;`
+          :
+            'background: #ffcc01;'
+        }
+        color: white;
+        border: 0;
+        border-radius: 50%;
+        width: 55px;
+        height: 55px;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      `}
+    >
+      { children }
+    </label>
+  );
+};
---
+++ src/components/atoms/button/index.js
@@ -0,0 +1,5 @@
+import FloatButton from './Float';
+
+export {
+  FloatButton,
+};
