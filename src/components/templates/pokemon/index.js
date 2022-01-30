5014c1601e0c8dd4229004b8a57e88a5294c9e8f
Fiqar Aprialim <fiqarapr@gmail.com>
2022-01-30 21:11:48 +0800

    refactor: pagination in index pokemon

--- src/components/templates/pokemon/index.js
+++ src/components/templates/pokemon/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BaseContainer, FlexContainer } from 'components/atoms/container';
-import { IconButton } from 'components/atoms/button';
+import { BaseContainer } from 'components/atoms/container';
 import { Navbar } from 'components/organisms/navbar';
-import { Paragraph, HeadingOne } from 'components/atoms/typography';
+import { HeadingOne } from 'components/atoms/typography';
 import { PokemonCards } from 'components/organisms/pokemon';
 import { Loader } from 'components/atoms/loader';
+import { BasePagination } from 'components/molecules/pagination';
 import PokemonLogo from 'assets/images/pokemon_logo.png';
 
 export default function PokemonIndex({ title, navbar, items, pagination }) {
@@ -59,31 +59,12 @@ export default function PokemonIndex({ title, navbar, items, pagination }) {
                 <BaseContainer
                   margin="15px 0px 0px 0px"
                 >
-                  <FlexContainer
-                    align="center"
-                    justify="center"
-                    gap="10px"
-                  >
-                    <IconButton
-                      clickEvent={pagination.prevPageEvent}
-                      disabled={pagination.currentPage === 1}
-                    >
-                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
-                      </svg>
-                    </IconButton>
-                    <Paragraph>
-                      { pagination.currentPage }
-                    </Paragraph>
-                    <IconButton
-                      clickEvent={pagination.nextPageEvent}
-                      disabled={pagination.currentPage === pagination.totalPage}
-                    >
-                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-                      </svg>
-                    </IconButton>
-                  </FlexContainer>
+                  <BasePagination 
+                    currentPage={pagination.currentPage}
+                    totalPage={pagination.totalPage}
+                    prevPageEvent={pagination.prevPageEvent}
+                    nextPageEvent={pagination.nextPageEvent}
+                  />
                 </BaseContainer>
                 : null
               }
